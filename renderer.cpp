c0a18152979e2d8e8cb9766a5fafb2c0d6ced2c5
Valery Shevchuk <shevchuk.valery@gmail.com>
2016-04-21 00:48:11 +0300

    Fix Bresenham's line algo implementation bug

--- renderer.cpp
+++ renderer.cpp
@@ -13,6 +13,12 @@ Renderer::~Renderer()
 
 void Renderer::drawLine(int x1, int y1, int x2, int y2, int color, ICanvas &canvas)
 {
+    const bool highSlope = abs(y2 - y1) > abs(x2 - x1);
+    if (highSlope) {
+        std::swap(x1, y1);
+        std::swap(x2, y2);
+    }
+
     if (x2 < x1) {
         std::swap(x1, x2);
         std::swap(y1, y2);
@@ -21,28 +27,19 @@ void Renderer::drawLine(int x1, int y1, int x2, int y2, int color, ICanvas &canv
     int dx = x2 - x1;
     int dy = y2 - y1;
 
-    const bool highSlope = abs(dy) > abs(dx);
-    if (highSlope) {
-        std::swap(x1, y1);
-        std::swap(x2, y2);
-        std::swap(dx, dy);
-    }
-
-    const int slope = dy;
-
     int currY = y1;
-    long errY = 0;
+    int errY = 0;
     for (int x = x1; x <= x2; ++x) {
         if (highSlope)
             canvas.setPixelColor(currY, x, color);
         else
             canvas.setPixelColor(x, currY, color);
 
-        errY += slope;
+        errY += 2 * abs(dy);
 
-        if (labs(errY) > abs(dx)) {
-            currY += (slope > 0 ? +1 : -1);
-            errY -= 2 * dy;
+        if (errY > dx) {
+            currY += (dy > 0 ? +1 : -1);
+            errY -= 2 * dx;
         }
     }
 }
