3e614e9335258fee9bfa866a27200cac64f07f12
Valery Shevchuk <shevchuk.valery@gmail.com>
2016-04-21 00:50:18 +0300

    Bootstrap script enhancements
    * die when some step is failed
    * pass all args from script to final executable

--- build_n_run.py
+++ build_n_run.py
@@ -3,14 +3,21 @@
 import os
 import os.path
 import subprocess
+import sys
 
 __build_dir = './build'
 
-if not os.path.isdir(__build_dir):
-    os.mkdir(__build_dir)
+def exec_or_die(cmd):
+    if subprocess.call(cmd, shell=True) != 0:
+        exit(1)
 
-project_dir = os.getcwd()
-os.chdir(__build_dir)
-subprocess.call('cmake {}'.format(project_dir), shell=True)
-subprocess.call('make -j16', shell=True)
-subprocess.call('./Software3dRenderer')
+if __name__ == '__main__':
+    if not os.path.isdir(__build_dir):
+        os.mkdir(__build_dir)
+
+    project_dir = os.getcwd()
+    os.chdir(__build_dir)
+
+    exec_or_die('cmake {}'.format(project_dir))
+    exec_or_die('make -j16')
+    exec_or_die(' '.join(['./Software3dRenderer'] + sys.argv[1:]))
